32d27bb3dd99b14d10e5eacabddf3e9ab95f42fe
Lsnap <lsnap_95@ymail.com>
2017-05-14 04:19:22 -0500

    Ajusticos varios

---
+++ php/asignar.php
@@ -0,0 +1,19 @@
+<?php
+include("../php/conexion.php");
+if(isset($_POST['id'])){
+		$id = $_POST['id'];
+	}
+
+	 $consulta = mysqli_query($conexion , "SELECT * FROM persona where Identificacion_Persona = '$id'" );
+	 if(!$consulta) {
+	    die("Error: no se pudo realizar la consulta");	
+	 	 while($fila = mysqli_fetch_assoc($consulta)) 
+	  		{ 
+	  			echo "<input type="text" name="id" value='"$fila['idPersona']"'>";
+	 	 	} 
+	 	 if (mysql_num_rows($resultado)==0) {
+	 	 	print "No existe el usuario en el sistema";
+	 	 }
+ 	}
+ 	header("location: ../asignacion.php"); 
+?>
